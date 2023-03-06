19f1b0fbcb449a253a55ad991bf065979922bb83
Gabriel Zafra <77026475+gabdrz@users.noreply.github.com>
2023-03-06 00:43:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,20 +5,21 @@ import instagram as ig
 import photoshop as ps
 
 SPREADSHEET = "(ORG) Missed Connections (Responses).xls"
+ID = "1rtcZwv9wBoxJokbiTW65BmlRvqSp_vkQFLG8IG4hNcE"
   
 # Initializing a GoogleAuth Object
 gauth = GoogleAuth()
-  
+
 # client_secrets.json file is verified
 # and it automatically handles authentication
 gauth.LocalWebserverAuth()
-  
+
 # GoogleDrive Instance is created using
 # authenticated GoogleAuth instance
 drive = GoogleDrive(gauth)
-  
+
 # Initialize GoogleDriveFile instance with file id
-file_obj = drive.CreateFile({'id': '1rtcZwv9wBoxJokbiTW65BmlRvqSp_vkQFLG8IG4hNcE'})
+file_obj = drive.CreateFile({'id': ID})
 file_obj.GetContentFile(SPREADSHEET,
                 mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
 dataframe = pd.read_excel(SPREADSHEET)
@@ -26,4 +27,3 @@ dataset = dataframe.to_numpy()
 
 ps.makePNG(dataset)
 ig.upload(len(dataset))
-
